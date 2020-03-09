65fd14697859059a690757423e0af03687dc2cac
Nicholas Jardine <nicholasjardine0@gmail.com>
2020-03-09 11:04:11 +0200

    models created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'home#index'
+  get 'categories/home'
+  get 'categories/garden'
+  get 'categories/art'
+  get 'home/index'
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
+  get 'pages/social_outreach'
+  get 'carts/show'
+
+  resources :order_items
+  resource :carts, only: [:show]
+
+  resources :categories, only: [:index, :show, :home, :art, :garden] do
+    resources :products, only: [:index, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
